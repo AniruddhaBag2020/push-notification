0df0980a5e22b9dff678c6b62eb3e3792dfd4632
Aniruddha Bag <aniruddha.bag17@gmail.com>
2021-06-02 21:38:44 +0530

    push

--- push.js
+++ push.js
@@ -1,11 +1,13 @@
 var push = require('web-push');
 
-let vapidKeys = push.generateVAPIDKeys();
+let vapidKeys = {
+    publicKey: 'BIKyZ40diPwGOLXNA3pZBV_MLlZ750yKZcuhZ9KioKNSPwwpkvRrGX8SY6cWHEAHTaJjgD_VhzDn-1gkrfq-gHE',  
+    privateKey: '6iDinp9jZEtSKfrClI2_6TWx_Qtd7cLM7YVLlTooS6g'
+  }
 
-console.log(vapidKeys);
 
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c89sWmfFF-s:APA91bG2mdB1T6FZt2iXTPNzedhv-JOiVCIAd4W-FqWdANmio7ARznNllozAZ3VtU6iJb37VNWycKOa5JqwpKppZNgPHYmU4rmi77NvOaJJAz7Wh49Yv9npxgJTjsf5_syCtng4wKhxr","expirationTime":null,"keys":{"p256dh":"BIVbkRjBOXt3PUxe4HR5j6nTsX7lj2W7p9gdTNmS0igfvymXpHkWgxWFeANpCfCOfoWXT1F-2l4tdOG3JsQxYl8","auth":"xVweFhUSWxvJvEvMgRPC2Q"}};
 
 push.sendNotification(sub, 'text messege');
