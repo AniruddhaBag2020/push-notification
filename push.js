848922464c6db7306a5081c5fe09585f776735e9
Aniruddha Bag <aniruddha.bag17@gmail.com>
2021-06-03 00:35:25 +0530

    updated key

--- push.js
+++ push.js
@@ -1,6 +1,7 @@
 var push = require('web-push');
 let vapidKeys = {
-    publicKey: 'BBxQH2GsF4so4echj-X_cy0CYtqQrDMFSPcyHZH5Ie4kUV37zluBZBXe7bSSADnNKfQvs788W2TALoUhCdtPzRY',  privateKey: 'ZrVaHKZciWK1FlC_T1t24xeHAHOI90fmoODEJBZoYWs'
+    publicKey: 'BBxQH2GsF4so4echj-X_cy0CYtqQrDMFSPcyHZH5Ie4kUV37zluBZBXe7bSSADnNKfQvs788W2TALoUhCdtPzRY',
+    privateKey: 'ZrVaHKZciWK1FlC_T1t24xeHAHOI90fmoODEJBZoYWs'
   }
 // let vapidKeys = {
 //     publicKey: 'BIKyZ40diPwGOLXNA3pZBV_MLlZ750yKZcuhZ9KioKNSPwwpkvRrGX8SY6cWHEAHTaJjgD_VhzDn-1gkrfq-gHE',  
@@ -12,4 +13,6 @@ push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.pr
 
 let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c89sWmfFF-s:APA91bG2mdB1T6FZt2iXTPNzedhv-JOiVCIAd4W-FqWdANmio7ARznNllozAZ3VtU6iJb37VNWycKOa5JqwpKppZNgPHYmU4rmi77NvOaJJAz7Wh49Yv9npxgJTjsf5_syCtng4wKhxr","expirationTime":null,"keys":{"p256dh":"BIVbkRjBOXt3PUxe4HR5j6nTsX7lj2W7p9gdTNmS0igfvymXpHkWgxWFeANpCfCOfoWXT1F-2l4tdOG3JsQxYl8","auth":"xVweFhUSWxvJvEvMgRPC2Q"}};
 
-push.sendNotification(sub, 'text messege');
+push.sendNotification(sub, 'text messege').catch(e =>{
+    console.log(e);
+});
